e2252c9fed5fb599937966beaae384334ec00f30
ekso <80045777+eksotopro@users.noreply.github.com>
2022-08-16 23:20:34 +0200

    Update Commands.lua

--- Urls/Commands.lua
+++ Urls/Commands.lua
@@ -1,1 +1,25 @@
-print("Soon...")
+LocalPlayer.Chatted:connect(function(cht)
+if cht:match(".reset") then
+game.Players.LocalPlayer.Character.Humanoid:Destroy()
+    end
+end)
+LocalPlaye.Chatted:connect(function(cht)
+if cht:match(".jobid") then
+setclipboard(game.JobId)
+    end
+end)
+LocalPlayer.Chatted:connect(function(cht)
+if cht:match(".hop") then
+loadstring(game:HttpGet('https://raw.githubLocalPlayercontent.com/eksotopro/holders/main/serverhop.lua'))()
+    end
+end)
+LocalPlayer.Chatted:connect(function(cht)
+if cht:match(".rejoin") then
+loadstring(game:HttpGet('https://pastebin.com/raw/1gtVMUz3'))()
+    end
+end)
+LocalPlayer.Chatted:connect(function(cht)
+if cht:match(".close") then
+game:Shutdown()
+    end
+end)
