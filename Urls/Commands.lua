8b0d55347fab0d5df3d3918f6a56963df74e4600
ekso <80045777+eksotopro@users.noreply.github.com>
2022-08-25 13:50:10 +0200

    Update Commands.lua

--- Urls/Commands.lua
+++ Urls/Commands.lua
@@ -1,26 +1,2 @@
 local LocalPlayer = game.Players.LocalPlayer
-LocalPlayer.Chatted:connect(function(cht)
-if cht:match(".reset") then
-game.Players.LocalPlayer.Character.Humanoid:Destroy()
-    end
-end)
-LocalPlayer.Chatted:connect(function(cht)
-if cht:match(".jobid") then
-setclipboard(game.JobId)
-    end
-end)
-LocalPlayer.Chatted:connect(function(cht)
-if cht:match(".hop") then
-loadstring(game:HttpGet('https://raw.githubLocalPlayercontent.com/eksotopro/holders/main/serverhop.lua'))()
-    end
-end)
-LocalPlayer.Chatted:connect(function(cht)
-if cht:match(".rejoin") then
-loadstring(game:HttpGet('https://pastebin.com/raw/1gtVMUz3'))()
-    end
-end)
-LocalPlayer.Chatted:connect(function(cht)
-if cht:match(".close") then
-game:Shutdown()
-    end
-end)
+LocalPlayer.Chatted:connect(function(cht) if cht:match(".reset") then game.Players.LocalPlayer.Character.Humanoid:Destroy() end end) LocalPlayer.Chatted:connect(function(cht) if cht:match(".jobid") then setclipboard(game.JobId) end end) LocalPlayer.Chatted:connect(function(cht) if cht:match(".hop") then loadstring(game:HttpGet('https://raw.githubLocalPlayercontent.com/eksotopro/holders/main/serverhop.lua'))() end end) LocalPlayer.Chatted:connect(function(cht) if cht:match(".rejoin") then loadstring(game:HttpGet('https://pastebin.com/raw/1gtVMUz3'))() end end) LocalPlayer.Chatted:connect(function(cht) if cht:match(".close") then game:Shutdown()end end) print("Commands loaded.")
