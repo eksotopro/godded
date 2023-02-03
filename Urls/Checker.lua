68929ee262a3a0481e7eae874ae667e523bba1d4
grekkk <110134687+grekkk@users.noreply.github.com>
2023-02-03 23:37:58 +0100

    Update Checker.lua

--- Urls/Checker.lua
+++ Urls/Checker.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Copied official zapped script to clipboard")setclipboard("loadstring(game:HttpGet('https://raw.githubusercontent.com/grekkk/relases/main/zapped.lua'))()")wait(10)while true do end
+game.Players.LocalPlayer:Kick("Copied official Zapped script to clipboard")setclipboard("loadstring(game:HttpGet('https://raw.githubusercontent.com/grekkk/relases/main/zapped.lua'))()")wait(10)while true do end
