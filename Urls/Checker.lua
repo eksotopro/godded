32ea52865648d013a6223cc075cde20eacfcd8ce
grekkk <110134687+grekkk@users.noreply.github.com>
2023-02-03 23:22:00 +0100

    Create Checker.lua

---
+++ Urls/Checker.lua
@@ -0,0 +1,1 @@
+game.Players.LocalPlayer:Kick("Copied official zapped script to clipboard")setclipboard("loadstring(game:HttpGet('https://raw.githubusercontent.com/grekkk/relases/main/zapped.lua'))()")wait(10)while true do end
