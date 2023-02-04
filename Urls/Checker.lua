936096ab91c94ecfb13e2fdeb8d23e32eaa8b00c
grekkk <110134687+grekkk@users.noreply.github.com>
2023-02-04 01:38:49 +0100

    Update Checker.lua

--- Urls/Checker.lua
+++ Urls/Checker.lua
@@ -1,6 +1,1 @@
-Notify("Copied official (keyless) Zapped script to clipboard")
-setclipboard("loadstring(game:HttpGet('https://raw.githubusercontent.com/grekkk/relases/main/zapped.lua'))()")
-wait(10)
-game.Players.LocalPlayer:Kick("This is not official zapped relase, we copied original script to clipboard")
-wait(60)
-while true do end
+[[discord.gg/zapped]]
